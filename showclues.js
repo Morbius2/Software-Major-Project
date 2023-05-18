f64397b33585a64fc11b81a769adbde6ba7b0be9
maxjenkinson58444 <131934514+maxjenkinson58444@users.noreply.github.com>
2023-05-18 20:05:17 +1000

    Delete showclues.js

--- showclues.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("CrosswordBuilder").addEventListener("click", Showclues); 
-
-function Showclues() {
-   var ShowClues = document.getElementById("CrosswordCluesText").value;
-   ShowClues = "Testing clues"
-   
-   
-}
